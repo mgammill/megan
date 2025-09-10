2e212eab6d1972b6d93fb4d49024525d13f16717
Megan <mgammill@berkeley.edu>
2025-09-10 16:32:11 -0700

    testingupload

---
+++ homework1/homework1.py
@@ -0,0 +1,29 @@
+# File: homework1. py
+
+# --- Variables and Data Types ---
+a = 10
+print(a)
+print(type(a))
+# a is an integer, a whole number with no decimals
+b = 1.5
+print(b)
+print(type(b))
+# b is a 'float,' a number with a decimal or fraction
+c = 3j
+print(c)
+print(type(c))
+# c is a 'complex,' a complex number with real and imaginary parts. 
+# In python, j represents i (j^2=-1), so the c is 3i.
+d = "hello"
+print(d)
+print(type(d))
+# d is a string, a data type that holds text
+e = [1, 2, 3]
+print(e)
+print(type(e))
+# e is a list of objects. 
+# In this case our list contains integrers.
+f = {"name": "Ellen", "favorite fruit": "strawberry"}
+print(f)
+print(type(f))
+# 
